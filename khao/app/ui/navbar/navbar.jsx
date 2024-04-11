9bf26dca5dacdecef5621d98d2a74f60d8242c53
sashdc <saharshdc@gmail.com>
2024-04-11 11:49:22 -0400

    added navbar

--- khao/app/ui/navbar/navbar.jsx
+++ khao/app/ui/navbar/navbar.jsx
@@ -2,13 +2,15 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center p-4 bg-gray-800 text-white">
-      {/* <h1 className="text-2xl">Khao</h1> */}
+    <nav className="flex justify-between items-center p-4 bg-gray-800 text-white" >
+      <h1 className="text-2xl">Khao</h1>
       <ul className="flex space-x-8">
-        <li href='/'>Home</li>
-        <li href='/menu'>Menu</li>
-        <li>About</li>
-        <li>Contact</li>
+        <Link href="/">
+          Home
+        </Link>
+        <Link href="/contact">
+          Contact
+        </Link>
         <Link href="/menu">
           Menu
         </Link>
